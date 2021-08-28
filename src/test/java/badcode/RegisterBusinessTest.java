788449ff2565f10e091aaba49f234e1e846b717d
Kachawut Sopa <kachawut.sop@ngernturbo.com>
2021-08-28 11:46:27 +0700

    test to fail

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -7,19 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterBusinessTest {
 
+    //fail
     @Test
     @DisplayName("Fail First name is required.")
     public void case101() {
         RegisterBusiness registerBusiness = new RegisterBusiness();
-        try {
-            registerBusiness.register(null,new Speaker());
-            fail();
-        } catch (ArgumentNullException e){
-            if(!e.getMessage().equals("First name is required.")){
-                fail();
-            }
+        ArgumentNullException thrown = assertThrows(
+                ArgumentNullException.class,
+                () -> registerBusiness.register(null, new Speaker())
+        );
+        assertEquals(thrown.getMessage(), "First name is required.");
+    }
 
-        }
+    @Test
+    @DisplayName("Fail Last name is required.")
+    public void case102() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Mk");
+        ArgumentNullException thrown = assertThrows(
+                ArgumentNullException.class,
+                () -> registerBusiness.register(null, speaker)
+        );
+        assertEquals(thrown.getMessage(), "Last name is required.");
+    }
+
+    @Test
+    @DisplayName("Fail Email is required.")
+    public void case103() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("M");
+        speaker.setLastName("k");
+        ArgumentNullException thrown = assertThrows(
+                ArgumentNullException.class,
+                () -> registerBusiness.register(null, speaker)
+        );
+        assertEquals(thrown.getMessage(), "Email is required.");
     }
 
 }
